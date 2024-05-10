f8ae3295b9d8aa8ce34dc7268055cdfe375f0ed3
yeha98552 <yeha98552@gmail.com>
2024-05-11 02:20:22 +0800

    refactor: Refactor data_query_dag to Airflow 2.0 (TaskFlow API)

--- airflow/dags/data_query_dag.py
+++ airflow/dags/data_query_dag.py
@@ -11,8 +11,7 @@ from utils.gcp import (
     upload_df_to_gcs,
 )
 
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
 RAW_BUCKET = os.environ.get("GCP_GCS_RAW_BUCKET")
@@ -33,119 +32,77 @@ default_args = {
 }
 
 
-def transform_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Transform data (drop duplicates, drop na, drop columns).
-
-    Args:
-        df (pd.DataFrame): input dataframe.
-
-    Returns:
-        pd.DataFrame: transformed dataframe.
-    """
-    # 使用python做一些轉換
-    df.drop_duplicates(inplace=True)
-    df.dropna(inplace=True)
-    df.drop(
-        columns=[
-            "airport_fee",
-            "congestion_surcharge",
-            "improvement_surcharge",
-            "extra",
-            "mta_tax",
-            "store_and_fwd_flag",
-        ],
-        inplace=True,
-    )
-    df.reset_index(drop=True, inplace=True)
-    return df[df["fare_amount"] < 10]
-
-
-def lookup_data(dataset_name, table_name):
-    """
-    Lookup data from bigquery.
-
-    Args:
-        dataset_name (str): dataset name.
-        table_name (str): table name.
-    """
-    query = f"""
-    SELECT *
-    FROM `{dataset_name}.{table_name}` WHERE fare_amount = 1
-    """
-    results = query_bq(query)
-    for row in results:
-        print(row)
-
-
-def join_data(dataset_name: str) -> pd.DataFrame:
-    """
-    Join data from bigquery.
-
-    Args:
-        dataset_name (str): dataset name.
-
-    Returns:
-        pd.DataFrame: joined dataframe.
-    """
-    # 這裡使用兩個相同的資料表名稱, 只是示範，應該是要兩個不同的資料表名稱操作才有意義
-    table1_name = TABLE_NAME
-    table2_name = TABLE_NAME
-    common_column = "VendorID"
-
-    query = f"""
-    SELECT t1.tpep_pickup_datetime, t1.trip_distance, t1.fare_amount,
-    FROM `{dataset_name}.{table1_name}` AS t1
-    INNER JOIN `{dataset_name}.{table2_name}` AS t2
-    ON t1.{common_column} = t2.{common_column}
-    WHERE t1.fare_amount = 1
-    """
-    df = query_bq_to_df(query)
-    return df
-
-
-dag = DAG(
-    "data_query_dag",
+@dag(
     default_args=default_args,
-    description="A DAG to process data, create BigQuery table, and perform table operations",
     schedule_interval="@daily",
+    start_date=days_ago(1),
+    catchup=False,
+    tags=["example"],
 )
+def data_query_dag():
+    @task
+    def download_data():
+        return download_df_from_gcs(bucket_name=RAW_BUCKET, blob_name=BLOB_NAME)
 
-with dag:
-    download_data_task = PythonOperator(
-        task_id="download_data_task",
-        python_callable=download_df_from_gcs,
-        op_kwargs={
-            "bucket_name": RAW_BUCKET,
-            "blob_name": BLOB_NAME,
-        },
-    )
+    @task
+    def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transform data (drop duplicates, drop na, drop columns).
 
-    transform_data_task = PythonOperator(
-        task_id="transform_data_task",
-        python_callable=transform_data,
-        op_args=[download_data_task.output],
-    )
+        Args:
+            df (pd.DataFrame): input dataframe.
 
-    upload_transformed_data_task = PythonOperator(
-        task_id="upload_transformed_data_task",
-        python_callable=upload_df_to_gcs,
-        op_kwargs={
-            "bucket_name": PROCESSED_BUCKET,
-            "blob_name": f"{TABLE_NAME}_processed",
-            "df": transform_data_task.output,
-        },
-    )
+        Returns:
+            pd.DataFrame: transformed dataframe.
+        """
+        # 使用python做一些轉換
+        df.drop_duplicates(inplace=True)
+        df.dropna(inplace=True)
+        df.drop(
+            columns=[
+                "airport_fee",
+                "congestion_surcharge",
+                "improvement_surcharge",
+                "extra",
+                "mta_tax",
+                "store_and_fwd_flag",
+            ],
+            inplace=True,
+        )
+        df.reset_index(drop=True, inplace=True)
+        return df[df["fare_amount"] < 10]
 
-    create_bq_external_table_task = PythonOperator(
-        task_id="create_bq_external_table_task",
-        python_callable=build_bq_from_gcs,
-        op_kwargs={
-            "dataset_name": BQ_ODS_DATASET,
-            "table_name": TABLE_NAME,
-            "bucket_name": PROCESSED_BUCKET,
-            "blob_name": f"{TABLE_NAME}_processed",
-            "schema": [
+    @task
+    def upload_transformed_data(df: pd.DataFrame, bucket_name, blob_name):
+        """
+        Upload transformed data to GCS.
+
+        Args:
+            df (pd.DataFrame): transformed dataframe.
+            bucket_name (str): bucket name.
+            blob_name (str): blob name.
+        """
+        upload_df_to_gcs(bucket_name=bucket_name, blob_name=blob_name, df=df)
+
+    @task
+    def create_bq_external_table(
+        bucket_name: str, blob_name: str, dataset_name: str, table_name: str
+    ):
+        """
+        Create BigQuery external table.
+
+        Args:
+            bucket_name (str): bucket name.
+            blob_name (str): blob name.
+            dataset_name (str): dataset name.
+            table_name (str): table name.
+        """
+        build_bq_from_gcs(
+            dataset_name=dataset_name,
+            table_name=table_name,
+            bucket_name=bucket_name,
+            blob_name=blob_name,
+            schema=[
                 bigquery.SchemaField(
                     "VendorID", "INTEGER", mode="REQUIRED"
                 ),  # NOT NULL
@@ -158,7 +115,7 @@ with dag:
                 bigquery.SchemaField("passenger_count", "FLOAT", mode="NULLABLE"),
                 bigquery.SchemaField("trip_distance", "FLOAT", mode="NULLABLE"),
                 bigquery.SchemaField("RatecodeID", "FLOAT", mode="NULLABLE"),
-                bigquery.SchemaField("PULocationID", "INTEGER", mode="NULLABLE"),
+                bigquery.SchemaField("PULocationID", "INTEGER", mode="REQUIRED"),
                 bigquery.SchemaField("DOLocationID", "INTEGER", mode="NULLABLE"),
                 bigquery.SchemaField("payment_type", "INTEGER", mode="NULLABLE"),
                 bigquery.SchemaField("fare_amount", "FLOAT", mode="NULLABLE"),
@@ -166,51 +123,82 @@ with dag:
                 bigquery.SchemaField("tolls_amount", "FLOAT", mode="NULLABLE"),
                 bigquery.SchemaField("total_amount", "FLOAT", mode="NULLABLE"),
             ],
-        },
-    )
+        )
 
-    lookup_bq_table_task = PythonOperator(
-        task_id="lookup_bq_table_task",
-        python_callable=lookup_data,
-        op_kwargs={
-            "dataset_name": BQ_ODS_DATASET,
-            "table_name": TABLE_NAME,
-        },
-    )
+    @task
+    def lookup_data(dataset_name: str, table_name: str):
+        """
+        Lookup data from bigquery.
 
-    join_data_task = PythonOperator(
-        task_id="join_data_task",
-        python_callable=join_data,
-        op_kwargs={
-            "dataset_name": BQ_ODS_DATASET,
-        },
-    )
+        Args:
+            dataset_name (str): dataset name.
+            table_name (str): table name.
+        """
+        query = f"SELECT * FROM `{dataset_name}.{table_name}` WHERE fare_amount = 1"
+        results = query_bq(query)
+        for row in results:
+            print(row)
 
-    upload_to_bigquery_task = PythonOperator(
-        task_id="upload_to_bigquery_task",
-        python_callable=upload_df_to_bq,
-        op_kwargs={
-            "dataset_name": BQ_DIM_DATASET,
-            "table_name": TABLE_NAME,
-            "df": join_data_task.output,
-            "schema": [
+    @task
+    def join_data() -> pd.DataFrame:
+        """
+        Join data from bigquery.
+
+        Args:
+            dataset_name (str): dataset name.
+
+        Returns:
+            pd.DataFrame: joined dataframe.
+        """
+        # 這裡使用兩個相同的資料表名稱, 只是示範，應該是要兩個不同的資料表名稱操作才有意義
+        query = f"""
+        SELECT t1.tpep_pickup_datetime, t1.trip_distance, t1.fare_amount
+        FROM `{BQ_ODS_DATASET}.{TABLE_NAME}` AS t1
+        INNER JOIN `{BQ_ODS_DATASET}.{TABLE_NAME}` AS t2
+        ON t1.VendorID = t2.VendorID
+        WHERE t1.fare_amount = 1
+        """
+        return query_bq_to_df(query)
+
+    @task
+    def upload_to_bigquery(df: pd.DataFrame):
+        """
+        Upload data to bigquery.
+
+        Args:
+            df (pd.DataFrame): dataframe.
+        """
+        upload_df_to_bq(
+            dataset_name=BQ_DIM_DATASET,
+            table_name=TABLE_NAME,
+            df=df,
+            schema=[
                 bigquery.SchemaField("tpep_pickup_datetime", "TIMESTAMP"),
                 bigquery.SchemaField("trip_distance", "FLOAT"),
                 bigquery.SchemaField("fare_amount", "FLOAT"),
             ],
-        },
-    )
+        )
 
     # 從GCS下載成pd.DataFrame，使用pandas做一些資料處理，再次上傳到GCS，最後建立BigQuery的Exteral Table
-    (
-        download_data_task
-        >> transform_data_task
-        >> upload_transformed_data_task
-        >> create_bq_external_table_task
+    data = download_data()
+    transformed_data = transform_data(data)
+    upload_transformed_data_task = upload_transformed_data(
+        transformed_data, PROCESSED_BUCKET, f"{TABLE_NAME}_processed"
     )
-
+    create_bq_external_table_task = create_bq_external_table(
+        PROCESSED_BUCKET, f"{TABLE_NAME}_processed", BQ_ODS_DATASET, TABLE_NAME
+    )
     # 查詢BigQuery的Exteral Table
-    create_bq_external_table_task >> lookup_bq_table_task
-
+    lookup_data_task = lookup_data(BQ_ODS_DATASET, TABLE_NAME)
     # 對兩個BigQuery的Exteral Table做join，最後上傳到BigQuery
-    create_bq_external_table_task >> join_data_task >> upload_to_bigquery_task
+    join_data_task = join_data()
+    upload_to_bigquery_task = upload_to_bigquery(join_data_task)
+
+    # Set dependencies
+    create_bq_external_table_task.set_upstream(upload_transformed_data_task)
+    lookup_data_task.set_upstream(create_bq_external_table_task)
+    join_data_task.set_upstream(create_bq_external_table_task)
+    upload_to_bigquery_task.set_upstream(join_data_task)
+
+
+data_query_dag()
