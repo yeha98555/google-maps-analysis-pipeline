cc74676dc7662e6fa7a4f26cd794fb58142c7c30
yeha98552 <yeha98552@gmail.com>
2024-05-10 19:57:02 +0800

    feat: Add upload_df_to_gcs and download_df_from_gcs

--- airflow/utils/gcp.py
+++ airflow/utils/gcp.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+
+import pandas as pd
 from google.cloud import bigquery, storage
 from google.cloud.exceptions import NotFound
 
@@ -6,6 +9,35 @@ from google.cloud.exceptions import NotFound
 storage.blob._MAX_MULTIPART_SIZE = 1024 * 1024  # 1 MB
 storage.blob._DEFAULT_CHUNKSIZE = 1024 * 1024  # 1 MB
 # End of Workaround
+
+
+def upload_df_to_gcs(bucket_name: str, blob_name: str, df: pd.DataFrame) -> bool:
+    """
+    Upload a pandas dataframe to GCS
+
+    Args:
+        bucket_name (str): The name of the bucket to upload to
+        blob_name (str): The name of the blob to upload to
+        df (pd.DataFrame): The dataframe to upload
+
+    Returns:
+        bool: True if the upload was successful, False otherwise
+    """
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+
+    blob = bucket.blob(blob_name)
+    if blob.exists():
+        print("File already exists in GCP.")
+        return False
+    try:
+        blob.upload_from_string(
+            df.to_parquet(), content_type="application/octet-stream"
+        )
+        print("Upload successful.")
+        return True
+    except Exception as e:
+        raise Exception(f"Failed to upload pd.DataFrame to GCS, reason: {e}")
 
 
 def upload_file_to_gcs(bucket_name: str, blob_name: str, source_filepath: str) -> bool:
@@ -33,6 +65,28 @@ def upload_file_to_gcs(bucket_name: str, blob_name: str, source_filepath: str) -
         return True
     except Exception as e:
         raise Exception(f"Failed to upload file to GCS, reason: {e}")
+
+
+def download_df_from_gcs(bucket_name: str, blob_name: str) -> pd.DataFrame:
+    """
+    Download a pandas dataframe from GCS
+
+    Args:
+        bucket_name (str): The name of the bucket to download from
+        blob_name (str): The name of the blob to download from
+
+    Returns:
+        pd.DataFrame: The dataframe downloaded from GCS
+    """
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+
+    blob = bucket.blob(blob_name)
+    if not blob.exists():
+        raise FileNotFoundError(f"file {blob_name} not found in bucket {bucket_name}")
+
+    bytes_data = blob.download_as_bytes()
+    return pd.read_parquet(BytesIO(bytes_data))
 
 
 def build_bq_from_gcs(
