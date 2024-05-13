a0dab298b0c7ee8d5514c26fb74208d2fa35d7e2
yeha98552 <yeha98552@gmail.com>
2024-05-14 02:09:15 +0800

    feat: Add gmaps time to_dim

---
+++ airflow/dags/d_gmaps_dim_time.py
@@ -0,0 +1,78 @@
+# # next dags
+# def trigger_dag_gmaps_reviews_ods_to_fct_reviews():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_fct_keywords():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_dim_users():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_dim_places():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_dim_time():
+#     pass
+
+
+import os
+from datetime import datetime, timedelta
+
+from google.cloud import bigquery
+from utils.gcp import query_bq
+
+from airflow.decorators import dag, task
+
+BQ_ODS_DATASET = os.environ.get("BIGQUERY_ODS_DATASET")
+BQ_DIM_DATASET = os.environ.get("BIGQUERY_DIM_DATASET")
+TABLE_NAME = "ods-gmaps_reviews"
+BQ_CLIENT = bigquery.Client()
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 5, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+
+@dag(
+    default_args=default_args,
+    schedule_interval=None,  # Because this is triggered by another DAG
+    catchup=False,
+    tags=["gmaps"],
+)
+def d_gmaps_dim_time():
+    @task
+    def l_dim_time(
+        src_dataset: str, src_table: str, dest_dataset: str, dest_table: str
+    ):
+        query = f"""
+        CREATE OR REPLACE TABLE `{dest_dataset}`.`{dest_table}` AS
+        SELECT
+          DISTINCT
+          FORMAT_DATE("%Y%m%d", published_at) AS date_id,
+          published_at AS date,
+          EXTRACT(YEAR FROM published_at) AS year,
+          EXTRACT(MONTH FROM published_at) AS moth,
+          EXTRACT(DAY FROM published_at) AS day,
+          CASE
+            WHEN EXTRACT(MONTH FROM published_at) IN (1, 2, 3) THEN 1
+            WHEN EXTRACT(MONTH FROM published_at) IN (4, 5, 6) THEN 2
+            WHEN EXTRACT(MONTH FROM published_at) IN (7, 8, 9) THEN 3
+            WHEN EXTRACT(MONTH FROM published_at) IN (10, 11, 12) THEN 4
+          END AS quarter
+        FROM
+          `{src_dataset}`.`{src_table}`
+        """
+        return query_bq(BQ_CLIENT, query)
+
+    l_dim_time(
+        src_dataset=BQ_ODS_DATASET,
+        src_table=TABLE_NAME,
+        dest_dataset=BQ_DIM_DATASET,
+        dest_table="dim-reviews_time",
+    )
+
+
+d_gmaps_dim_time()
