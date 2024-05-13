5cb9605fc2b0f2a4f86a5de37bac7e6cfed80b3c
yeha98552 <yeha98552@gmail.com>
2024-05-14 01:56:05 +0800

    feat: Add gmaps users to_dim

---
+++ airflow/dags/d_gmaps_dim_users.py
@@ -0,0 +1,69 @@
+# # next dags
+# def trigger_dag_gmaps_reviews_ods_to_fct_reviews():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_fct_keywords():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_dim_users():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_dim_places():
+#     pass
+
+# def trigger_dag_gmaps_reviews_ods_to_dim_time():
+#     pass
+
+
+import os
+from datetime import datetime, timedelta
+
+from google.cloud import bigquery
+from utils.gcp import query_bq
+
+from airflow.decorators import dag, task
+
+BQ_ODS_DATASET = os.environ.get("BIGQUERY_ODS_DATASET")
+BQ_DIM_DATASET = os.environ.get("BIGQUERY_DIM_DATASET")
+TABLE_NAME = "ods-gmaps_reviews"
+BQ_CLIENT = bigquery.Client()
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 5, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+
+@dag(
+    default_args=default_args,
+    schedule_interval=None,  # Because this is triggered by another DAG
+    catchup=False,
+    tags=["gmaps"],
+)
+def d_gmaps_user_dim():
+    @task
+    def l_dim_users(
+        src_dataset: str, src_table: str, dest_dataset: str, dest_table: str
+    ):
+        query = f"""
+        CREATE OR REPLACE TABLE `{dest_dataset}`.`{dest_table}` AS
+        SELECT DISTINCT
+          `user_name`,
+          `user_is_local_guide`,
+          `user_url`,
+        FROM
+          `{src_dataset}`.`{src_table}`
+        """
+        return query_bq(BQ_CLIENT, query)
+
+    l_dim_users(
+        src_dataset=BQ_ODS_DATASET,
+        src_table=TABLE_NAME,
+        dest_dataset=BQ_DIM_DATASET,
+        dest_table="dim-reviews_users",
+    )
+
+
+d_gmaps_user_dim()
