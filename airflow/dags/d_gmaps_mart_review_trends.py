98d34260a81ba9a8191d7c35bd832c54598e444c
yeha98552 <yeha98552@gmail.com>
2024-05-15 15:21:57 +0800

    feat: Add reviews trends mart

---
+++ airflow/dags/d_gmaps_mart_review_trends.py
@@ -0,0 +1,62 @@
+import os
+from datetime import datetime, timedelta
+
+from google.cloud import bigquery
+from utils.gcp import query_bq
+
+from airflow.decorators import dag, task
+
+BQ_DIM_DATASET = os.environ.get("BIGQUERY_DIM_DATASET")
+BQ_FACT_DATASET = os.environ.get("BIGQUERY_FACT_DATASET")
+BQ_MART_DATASET = os.environ.get("BIGQUERY_MART_DATASET")
+TABLE_NAME = "mart-reviews_trends"
+BQ_CLIENT = bigquery.Client()
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 5, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+
+@dag(
+    default_args=default_args,
+    schedule_interval=None,  # Because this is triggered by another DAG
+    catchup=False,
+    tags=["gmaps"],
+)
+def d_gmaps_mart_review_trends():
+    @task
+    def l_mart_review_trends(dest_dataset: str, dest_table: str):
+        query = f"""
+        CREATE OR REPLACE TABLE `{dest_dataset}`.`{dest_table}` AS
+        SELECT
+          p.`place_name`,
+          t.`year`,
+          t.`month`,
+          COUNT(r.`review_id`) AS `total_reviews`,
+          ROUND(AVG(r.`rating`), 2) AS `avg_rating`,
+        FROM
+          `{BQ_FACT_DATASET}`.`fact-reviews` r
+        JOIN
+          `{BQ_DIM_DATASET}`.`dim-reviews_places` p
+          ON r.`place_id` = p.`place_id`
+        JOIN
+          `{BQ_DIM_DATASET}`.`dim-reviews_time` t
+          ON r.`published_at` = t.`date`
+        GROUP BY
+          p.`place_name`,
+          t.`year`,
+          t.`month`
+        """
+        query_bq(BQ_CLIENT, query)
+        return f"{dest_table} created."
+
+    l_mart_review_trends(
+        dest_dataset=BQ_MART_DATASET,
+        dest_table=TABLE_NAME,
+    )
+
+
+d_gmaps_mart_review_trends()
