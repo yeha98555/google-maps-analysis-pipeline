04caff2544befdb2c1d9814d9547151a145250f2
yeha98552 <yeha98552@gmail.com>
2024-05-16 04:32:21 +0800

    feat: Add tripadvisor from src to ods

--- airflow/utils/common.py
+++ airflow/utils/common.py
@@ -1,5 +1,7 @@
 import hashlib
 
+from fuzzywuzzy import process
+
 
 def rename_place_id(place_name: str) -> str:
     """
@@ -12,4 +14,13 @@ def rename_place_id(place_name: str) -> str:
     Returns:
         str: The hash value of the place name
     """
-    return "place_" + hashlib.md5(place_name.encode("utf-8")).hexdigest()
+    return hashlib.md5(place_name.encode("utf-8")).hexdigest()
+
+
+def mapping_place_id(place_name: str, df) -> str:
+    best_match, best_score, *_ = process.extractOne(place_name, df["attraction_name"])
+
+    if best_match:
+        return df[df["attraction_name"] == best_match]["attraction_id"].values[0]
+
+    return None
