f6a8ef5d92e086c8b9bcfaab7bf2aadaf1478875
yeha98552 <yeha98552@gmail.com>
2024-05-16 04:49:28 +0800

    feat: Add fuzzy compare to get attraction_id in places to_ods

--- airflow/utils/common.py
+++ airflow/utils/common.py
@@ -1,5 +1,7 @@
 import hashlib
 
+from fuzzywuzzy import process
+
 
 def rename_place_id(place_name: str) -> str:
     """
@@ -13,3 +15,12 @@ def rename_place_id(place_name: str) -> str:
         str: The hash value of the place name
     """
     return hashlib.md5(place_name.encode("utf-8")).hexdigest()
+
+
+def mapping_place_id(place_name: str, df) -> str:
+    best_match, best_score, *_ = process.extractOne(place_name, df["attraction_name"])
+
+    if best_match:
+        return df[df["attraction_name"] == best_match]["attraction_id"].values[0]
+
+    return None
