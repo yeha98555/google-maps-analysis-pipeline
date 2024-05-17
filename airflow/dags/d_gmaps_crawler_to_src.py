1e6b1e0f907cb0718552708be893ceb994aa351d
yeha98552 <yeha98552@gmail.com>
2024-05-18 01:10:46 +0800

    feat: Update expand crawler to batch due to max_map_length is 1024

--- airflow/dags/d_gmaps_crawler_to_src.py
+++ airflow/dags/d_gmaps_crawler_to_src.py
@@ -31,7 +31,7 @@ default_args = {
 )
 def d_gmaps_crawler_to_src():
     @task
-    def get_attraction_list() -> list[dict]:
+    def get_attraction_list() -> list[list[dict]]:
         query = f"""
             SELECT DISTINCT
                 attraction_id,
@@ -40,43 +40,50 @@ def d_gmaps_crawler_to_src():
                 `{BQ_ODS_DATASET}`.`ods_tripadvisor_info`
         """
         df = BQ_CLIENT.query(query).to_dataframe()
-        return df.to_dict(orient="records")
+        attractions = df.to_dict(orient="records")
+        # batch
+        batch_size = 1024  # default max_length is 1024
+        return [
+            attractions[i : i + batch_size]
+            for i in range(0, len(attractions), batch_size)
+        ]
 
     @task
-    def run_gmaps_crawler(attraction: dict):
-        print(f"crawling attraction: {attraction}")
-        attraction_name = attraction["attraction_name"]
-        attraction_id = attraction["attraction_id"]
-        crawler_task = DockerOperator(
-            task_id=f"crawl_{attraction_name}",
-            image="gmaps-scraper",
-            api_version="auto",
-            auto_remove=True,
-            environment={
-                "ATTRACTION_ID": attraction_id,
-                "ATTRACTION_NAME": attraction_name,
-                "GCS_BUCKET_NAME": RAW_BUCKET,
-                "GCS_BLOB_NAME": "gmaps-taiwan",
-            },
-            command="make run",
-            mounts=[
-                Mount(
-                    source=CRAWER_GOOGLE_CREDENTIALS_LOCAL_PATH,
-                    target="/app/crawler_gcp_keyfile.json",
-                    type="bind",
-                    read_only=True,
-                ),
-            ],
-            mount_tmp_dir=False,
-            mem_limit="24g",
-            shm_size="2g",
-            docker_url="tcp://docker-proxy:2375",
-            network_mode="bridge",
-        )
-        return crawler_task.execute({})
+    def run_gmaps_crawler(batch: list[dict]):
+        for attraction in batch:
+            print(f"crawling attraction: {attraction}")
+            attraction_name = attraction["attraction_name"]
+            attraction_id = attraction["attraction_id"]
+            crawler_task = DockerOperator(
+                task_id=f"crawl_{attraction_id}",
+                image="gmaps-scraper",
+                api_version="auto",
+                auto_remove=True,
+                environment={
+                    "ATTRACTION_ID": attraction_id,
+                    "ATTRACTION_NAME": attraction_name,
+                    "GCS_BUCKET_NAME": RAW_BUCKET,
+                    "GCS_BLOB_NAME": "gmaps-taiwan",
+                },
+                command="make run",
+                mounts=[
+                    Mount(
+                        source=CRAWER_GOOGLE_CREDENTIALS_LOCAL_PATH,
+                        target="/app/crawler_gcp_keyfile.json",
+                        type="bind",
+                        read_only=True,
+                    ),
+                ],
+                mount_tmp_dir=False,
+                mem_limit="24g",
+                shm_size="2g",
+                docker_url="tcp://docker-proxy:2375",
+                network_mode="bridge",
+            )
+            crawler_task.execute({})
 
-    attractions = get_attraction_list()
-    run_gmaps_crawler.expand(attraction=attractions)
+    batches = get_attraction_list()
+    run_gmaps_crawler.expand(batch=batches)
 
 
 d_gmaps_crawler_to_src()
