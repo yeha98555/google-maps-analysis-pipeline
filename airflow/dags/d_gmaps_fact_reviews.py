ae662b162a1d720e179073a5e8193e9022e6b97b
yeha98552 <yeha98552@gmail.com>
2024-05-26 18:38:07 +0800

    feat: Remove emotion analysis

--- airflow/dags/d_gmaps_fact_reviews.py
+++ airflow/dags/d_gmaps_fact_reviews.py
@@ -1,17 +1,16 @@
 import os
 from datetime import datetime, timedelta
 
-import jieba
 import pandas as pd
 from google.cloud import bigquery
-from snownlp import SnowNLP
-from utils.gcp import query_bq_to_df, upload_df_to_bq
+from utils.gcp import query_bq
 
 from airflow.decorators import dag, task
 
 BQ_ODS_DATASET = os.environ.get("BIGQUERY_ODS_DATASET")
 BQ_FACT_DATASET = os.environ.get("BIGQUERY_FACT_DATASET")
-TABLE_NAME = "ods-gmaps_reviews"
+ODS_TABLE_NAME = "ods-gmaps_reviews"
+FACT_TABLE_NAME = "fact-reviews"
 BQ_CLIENT = bigquery.Client()
 
 default_args = {
@@ -20,39 +19,6 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
-
-stopwords = set(["的", "是", "在", "我"])
-
-
-def preprocess_text(text: str) -> str:
-    """
-    Preprocess text: cut words and remove stopwords
-
-    Args:
-        text (str): The text to be processed
-    Returns:
-        str: The processed text
-    """
-    words = jieba.cut(text)
-    filtered_words = [word for word in words if word not in stopwords]
-    return " ".join(filtered_words)
-
-
-def analyze_sentiment(text: str) -> float:
-    """
-    Analyze sentiment of the text
-
-    Args:
-        text (str): The text to be analyzed
-    Returns:
-        float: The sentiment score
-    """
-    text = preprocess_text(text)
-    if not text.strip():
-        return 0
-    s = SnowNLP(text)
-    # close to 1 is positive, close to 0 is negative
-    return round(s.sentiments, 2)
 
 
 @dag(
@@ -63,10 +29,12 @@ def analyze_sentiment(text: str) -> float:
 )
 def d_gmaps_fact_reviews():
     @task
-    def e_load_reviews(
+    def etl_load_reviews(
         src_dataset: str, src_table: str, dest_dataset: str, dest_table: str
     ) -> pd.DataFrame:
         query = f"""
+        CREATE OR REPLACE TABLE `{dest_dataset}.{dest_table}`
+        AS
         SELECT DISTINCT
           `review_id`,
           `place_name`,
@@ -77,37 +45,9 @@ def d_gmaps_fact_reviews():
         FROM
           `{src_dataset}`.`{src_table}`
         """
-        return query_bq_to_df(client=BQ_CLIENT, sql_query=query)
+        query_bq(client=BQ_CLIENT, sql_query=query)
 
-    @task
-    def t_reviews_emotion_score(df: pd.DataFrame) -> pd.DataFrame:
-        # if review_text null emotion is 0, else process analyze_sentiment
-        df["emotion_score"] = df["review_text"].apply(
-            lambda x: 0 if pd.isnull(x) else analyze_sentiment(x)
-        )
-        return df
-
-    @task
-    def l_fact_reviews(df: pd.DataFrame, dest_dataset: str, dest_table: str):
-        upload_df_to_bq(
-            BQ_CLIENT,
-            df,
-            dest_dataset,
-            dest_table,
-            partition_by="published_at",
-            schema=[
-                bigquery.SchemaField("review_id", "STRING", mode="REQUIRED"),
-                bigquery.SchemaField("place_name", "STRING", mode="REQUIRED"),
-                bigquery.SchemaField("user_name", "STRING", mode="REQUIRED"),
-                bigquery.SchemaField("rating", "INTEGER", mode="REQUIRED"),
-                bigquery.SchemaField("published_at", "DATE", mode="REQUIRED"),
-                bigquery.SchemaField("emotion_score", "FLOAT", mode="REQUIRED"),
-            ],
-        )
-
-    df = e_load_reviews(BQ_ODS_DATASET, TABLE_NAME, BQ_FACT_DATASET, "fact-reviews")
-    df_transformed = t_reviews_emotion_score(df)
-    l_fact_reviews(df_transformed, BQ_FACT_DATASET, "fact-reviews")
+    etl_load_reviews(BQ_ODS_DATASET, ODS_TABLE_NAME, BQ_FACT_DATASET, FACT_TABLE_NAME)
 
 
 d_gmaps_fact_reviews()
