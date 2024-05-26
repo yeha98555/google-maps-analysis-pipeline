2a53a07de264b4562377fb35a7007b988ae35fd3
yeha98552 <yeha98552@gmail.com>
2024-05-26 18:59:25 +0800

    feat: Add ExternalTaskSensor and remove emotion score

--- airflow/dags/d_gmaps_mart_review_trends.py
+++ airflow/dags/d_gmaps_mart_review_trends.py
@@ -5,6 +5,8 @@ from google.cloud import bigquery
 from utils.gcp import query_bq
 
 from airflow.decorators import dag, task
+from airflow.operators.dummy import DummyOperator
+from airflow.sensors.external_task import ExternalTaskSensor
 
 BQ_DIM_DATASET = os.environ.get("BIGQUERY_DIM_DATASET")
 BQ_FACT_DATASET = os.environ.get("BIGQUERY_FACT_DATASET")
@@ -22,11 +24,41 @@ default_args = {
 
 @dag(
     default_args=default_args,
-    schedule_interval=None,  # Because this is triggered by another DAG
+    schedule_interval="@daily",
     catchup=False,
     tags=["gmaps"],
 )
 def d_gmaps_mart_review_trends():
+    wait_for_fact_reviews = ExternalTaskSensor(
+        task_id="wait_for_fact_reviews",
+        external_dag_id="d_gmaps_fact_reviews",
+        external_task_id="etl_load_reviews",
+        poke_interval=120,  # 每 120 秒檢查一次
+        timeout=3600,  # 總等待時間為 3600 秒
+        mode="poke",
+    )
+
+    wait_for_dim_users = ExternalTaskSensor(
+        task_id="wait_for_dim_users",
+        external_dag_id="d_gmaps_dim_users",
+        external_task_id="l_dim_users",
+        poke_interval=120,
+        timeout=3600,
+        mode="poke",
+    )
+
+    wait_for_dim_time = ExternalTaskSensor(
+        task_id="wait_for_dim_time",
+        external_dag_id="d_gmaps_dim_time",
+        external_task_id="l_dim_time",
+        poke_interval=120,
+        timeout=3600,
+        mode="poke",
+    )
+
+    # Dummy task to synchronize after all sensors
+    all_sensors_complete = DummyOperator(task_id="all_sensors_complete")
+
     @task
     def l_mart_review_trends(dest_dataset: str, dest_table: str):
         query = f"""
@@ -45,7 +77,6 @@ def d_gmaps_mart_review_trends():
           t.`date`,
           COUNT(r.`review_id`) AS `total_reviews`,
           ROUND(AVG(r.`rating`), 2) AS `avg_rating`,
-          ROUND(AVG(r.`emotion_score`), 2) AS `avg_emotion_score`,
         FROM
           `{BQ_FACT_DATASET}`.`fact-reviews` r
         JOIN
@@ -70,9 +101,13 @@ def d_gmaps_mart_review_trends():
         query_bq(BQ_CLIENT, query)
         return f"{dest_table} created."
 
-    l_mart_review_trends(
-        dest_dataset=BQ_MART_DATASET,
-        dest_table=TABLE_NAME,
+    (
+        [wait_for_fact_reviews, wait_for_dim_users, wait_for_dim_time]
+        >> all_sensors_complete
+        >> l_mart_review_trends(
+            dest_dataset=BQ_MART_DATASET,
+            dest_table=TABLE_NAME,
+        )
     )
 
 
