a0fe75c635e1c3f75991c713fa4075ba6cb5cc0c
yeha98555 <yeha98555@gmail.com>
2024-06-03 12:27:35 +0800

    build: Add bq connection delegate to airflow for creating remote UDF

--- terraform/main.tf
+++ terraform/main.tf
@@ -146,6 +146,12 @@ resource "google_project_iam_member" "airflow_bq_connection_user" {
   member  = "serviceAccount:${google_service_account.airflow.email}"
 }
 
+resource "google_project_iam_member" "airflow_bq_connection_delegate" {
+  project = var.project_id
+  role    = "roles/bigquery.connectionAdmin"
+  member  = "serviceAccount:${google_service_account.airflow.email}"
+}
+
 resource "google_service_account" "viewer_sa" {
   account_id   = "viewer-account"
   display_name = "Viewer Service Account"
